c66bcae80f9ca6d8fea55a6e3752ee864a63b066
Dan Palmere <danpalmere356@gmail.com>
2026-09-23 23:04:45 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kingdom-bible-v2';
+const CACHE_NAME = 'kingdom-bible-v4';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -13,6 +13,17 @@ self.addEventListener('install', (event) => {
     })
   );
   self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
