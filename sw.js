2ac0f03e4139e722d0e1597a6fcbaaa578ea3105
Dan Palmere <danpalmere356@gmail.com>
2026-09-23 22:49:08 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'kingdom-bible-v1';
+const CACHE_NAME = 'kingdom-bible-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './data/bsb_nt.json'
 ];
 
-// Install Event: Save core files locally
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -15,7 +15,6 @@ self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
-// Serve directly from local device cache first, fallback to internet
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
